1cffd34aae25cc4633ea6f89307bbce4e9c88ef5
Benoit Hediard <ben@agorapulse.com>
2016-04-20 14:21:55 +0200

    Import cleaning [skip ci]

--- grails-app/services/grails/plugin/awssdk/s3/AmazonS3Service.groovy
+++ grails-app/services/grails/plugin/awssdk/s3/AmazonS3Service.groovy
@@ -10,7 +10,6 @@ import grails.core.GrailsApplication
 import grails.plugin.awssdk.AwsClientUtil
 import groovy.util.logging.Log4j
 import org.springframework.beans.factory.InitializingBean
-import org.springframework.web.multipart.MultipartFile
 
 @Log4j
 class AmazonS3Service implements InitializingBean {
