395d3844098275e25ff918b4a8d572ed9d3ec32e
Jean-Vincent Drean <jv@drean.fr>
2016-11-04 10:50:30 +0100

    Connection and read timeout handling, GZIP handling

--- grails-app/services/grails/plugin/awssdk/util/UploadService.groovy
+++ grails-app/services/grails/plugin/awssdk/util/UploadService.groovy
@@ -3,15 +3,18 @@ package grails.plugin.awssdk.util
 import grails.core.GrailsApplication
 import org.springframework.web.multipart.MultipartFile
 
+import java.util.zip.GZIPInputStream
+
 class UploadService {
+
+    static lazyInit = false
+    static transactional = false
+
+    static int CONNECT_TIMEOUT = 15000
+    static int READ_TIMEOUT = 60000
 
     GrailsApplication grailsApplication
 
-    /**
-     *
-     * @param urlString
-     * @return
-     */
     File downloadFile(String urlString) {
         if (!urlString) {
             return null
@@ -24,6 +27,8 @@ class UploadService {
 
             // Detect potential redirects (Code 3xx)
             HttpURLConnection connection = (HttpURLConnection) url.openConnection()
+            connection.connectTimeout = CONNECT_TIMEOUT
+            connection.readTimeout = READ_TIMEOUT
             int status = connection.getResponseCode();
             if (status == HttpURLConnection.HTTP_MOVED_TEMP
                     || status == HttpURLConnection.HTTP_MOVED_PERM
@@ -32,10 +37,24 @@ class UploadService {
             }
             connection.disconnect()
 
-            out << url.openStream()
+            URLConnection redirectConnection = url.openConnection()
+            redirectConnection.connectTimeout = CONNECT_TIMEOUT
+            redirectConnection.readTimeout = READ_TIMEOUT
+
+            // uncompressing if needed
+            if (redirectConnection.contentEncoding == 'gzip') {
+                GZIPInputStream gzipInputStream = new GZIPInputStream(redirectConnection.inputStream)
+                out << gzipInputStream
+            } else {
+                out << redirectConnection.inputStream
+            }
+
             out.close()
             File file = new File("$uploadPath/$fileName")
             return file
+        } catch (SocketTimeoutException exception) {
+            log.warn exception
+            return null
         } catch (FileNotFoundException exception) {
             log.warn(exception)
             return null
@@ -45,24 +64,12 @@ class UploadService {
         }
     }
 
-    /**
-     *
-     * @param multipartFile
-     * @param fileName
-     */
-    void uploadFile(MultipartFile multipartFile,
-                    fileName) {
-        multipartFile.transferTo(new File("${uploadPath}/${fileName}"))
+    String getUploadPath() {
+        grailsApplication.config.agorapulse?.uploadPath ?: '/var/tmp'
     }
 
-    // PRIVATE
-
-    def getConfig() {
-        grailsApplication.config.grails?.plugins?.awssdk ?: grailsApplication.config.grails?.plugin?.awssdk
-    }
-
-    private String getUploadPath() {
-        config['s3'] ? config['s3'].uploadPath ?: '/var/tmp' : '/var/tmp'
-    }
+	void uploadFile(MultipartFile multipartFile, fileName) {
+		multipartFile.transferTo(new File("${uploadPath}/${fileName}"))
+	}
 
 }
